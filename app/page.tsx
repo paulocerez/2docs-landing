96285fc87ffc8eca9904761d6bc75d17c0a3a9a1
paulocerez <paulo.ramirez@web.de>
2024-08-27 16:00:35 +0200

    feat: implement shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex flex-col space-y-12 justify-center items-center p-24 text-center h-screen">
       <h1 className="text-4xl">2docs</h1>
       <p>
-        Combine two or more API docs into seamless workflows. Zapier for code
-        generation.
+        Combine two or more API docs into seamless workflows. Best used for all
+        API&apos;s Zapier didn&apos;t integrate yet.
       </p>
-      <button className="border border-l-white px-4 py-2 rounded-md hover:bg-gray-700">
-        Sign up for the Waitlist
-      </button>
+      <Button variant="outline">Sign up for the Waitlist</Button>
     </main>
   );
 }
