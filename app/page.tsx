b4b5304164eb38ebf8b0572017f9bdc49fb35eb8
paulocerez <paulo.ramirez@web.de>
2024-08-27 16:30:41 +0200

    feat: shadcn themeprovider and toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/modeToggle";
 
 export default function Home() {
   return (
-    <main className="flex flex-col space-y-12 justify-center items-center p-24 text-center h-screen">
+    <main className="flex flex-col space-y-12 items-center p-24 text-center h-screen">
+      <div className="flex flex-row items-end">
+        <ModeToggle />
+      </div>
       <h1 className="text-4xl">2docs</h1>
       <p>
         Combine two or more API docs into seamless workflows. Best used for all
