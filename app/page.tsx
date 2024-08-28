a35745fb4ea501618fc6876c88b1589fbc053948
paulocerez <paulo.ramirez@web.de>
2024-08-28 08:49:37 +0200

    feat: add header for mode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
+import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/modeToggle";
 
 export default function Home() {
   return (
-    <main className="flex flex-col space-y-12 items-center p-24 text-center h-screen">
-      <div className="flex flex-row items-end">
+    <main className="flex flex-col space-y-12 items-center p-12 text-center h-screen">
+      <div className="flex flex-row justify-end w-full">
+        <Header />
         <ModeToggle />
       </div>
       <h1 className="text-4xl">2docs</h1>
       <p>
-        Combine two or more API docs into seamless workflows. Best used for all
+        Combine two or more API docs into seamless workflows. Best used for all2
         API&apos;s Zapier didn&apos;t integrate yet.
       </p>
       <Button variant="outline">Sign up for the Waitlist</Button>
