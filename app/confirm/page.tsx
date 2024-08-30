22ca976cea7abe3d0937e79c7cb5719612991a6b
paulocerez <paulo.ramirez@web.de>
2024-08-30 21:31:37 +0200

    fix: customize confirmation page

--- app/confirm/page.tsx
+++ app/confirm/page.tsx
@@ -1,14 +1,17 @@
 export default function ConfirmPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Subscription Confirmed!</h1>
-      <p className="text-xl mb-4">
-        Thank you for confirming your subscription to our waitlist.
+    <div className="flex min-h-screen flex-col items-center justify-center text-center py-16 px-12 md:px-24">
+      <h1 className="text-xl md:text-3xl font-bold mb-8">
+        Subscription <span className="underline text-blue-500">confirmed</span>!
+        😎
+      </h1>
+      <p className="text-lg mb-4">
+        Thank you for confirming your subscription to the 2docs waitlist.
+        <br />
+        I&apos;ll keep you updated on our progress and notify you when I launch
+        👉🏼👈🏼.
       </p>
-      <p className="text-lg">
-        We&apos;ll keep you updated on our progress and notify you when we
-        launch.
-      </p>
+      <p>Best, Paulo 👋🏼</p>
     </div>
   );
 }
