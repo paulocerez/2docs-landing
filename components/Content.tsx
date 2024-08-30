60f9469b7697b8dab9dc42e033840a401337cd3b
paulocerez <paulo.ramirez@web.de>
2024-08-30 14:54:25 +0200

    fix: adjust theming for correct graphic display

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,20 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import Signup from "./SignupForm";
 import Image from "next/image";
 import { useTheme } from "next-themes";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Content() {
   const [signup, setSignup] = useState(false);
-  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme } = useTheme();
+  //   resolvedTheme of useTheme hook takes user preference + system settings into account
+
+  //   mounted state set in useEffect to ensure rendering only after being mounted on client side
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   if (signup) {
     return <Signup />;
   }
 
+  //
+  if (!mounted) {
+    return (
+      <div className="flex flex-col space-y-3">
+        <Skeleton className="h-[300px] w-[450px] rounded-xl" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[450px]" />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col space-y-16 items-center text-center animate-slide-in-bottom max-w-3xl">
-      {theme === "dark" ? (
+      {resolvedTheme === "dark" ? (
         <Image src="/waiter-dark.svg" alt="overview" width={450} height={300} />
       ) : (
         <Image
